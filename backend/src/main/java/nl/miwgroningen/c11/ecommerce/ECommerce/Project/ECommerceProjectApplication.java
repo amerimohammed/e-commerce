80a4a2793f6b336a203a5f6ed08fda02f101c545
Mohammed Alameri <mohammedagl6@gmail.com>
2023-08-19 12:10:17 +0200

    initial commit

---
+++ backend/src/main/java/nl/miwgroningen/c11/ecommerce/ECommerce/Project/ECommerceProjectApplication.java
@@ -0,0 +1,13 @@
+package nl.miwgroningen.c11.ecommerce.ECommerce.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceProjectApplication.class, args);
+	}
+
+}
